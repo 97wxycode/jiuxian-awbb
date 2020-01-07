afd06ed54d05b6c5a9cc2dfc1c17e9a702093678
97wxycode <599855238@qq.com>
2020-01-07 22:40:57 +0800

    index 50%

--- pages/index/components/shop-item/shop-item.js
+++ pages/index/components/shop-item/shop-item.js
@@ -1,4 +1,4 @@
-// pages/index/components/activity-count/activity-count.js
+// pages/index/components/shop-item/shop-item.js
 Component({
   /**
    * 组件的属性列表
@@ -18,9 +18,6 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    clickHandler(e){
-      const query = wx.createSelectorQuery().in(this)
-      console.log(query)
-    }
+
   }
 })
