cb44a4ccdaa8552de65c6e0f4f98236955c4678c
97wxycode <599855238@qq.com>
2020-01-08 22:44:25 +0800

    wxy-index90%

--- pages/index/components/shop-item/shop-item.js
+++ pages/index/components/shop-item/shop-item.js
@@ -1,23 +1,25 @@
-// pages/index/components/shop-item/shop-item.js
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
-
+    isborder:{
+      type:Boolean,
+      value:true
+    }
   },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    isBorder:true
   },
-
-  /**
-   * 组件的方法列表
-   */
+  lifetimes:{
+    attached(){
+      this.setData({
+        isBorder: this.data.isborder
+      });
+    }
+  },
   methods: {
-
+    goBuy(){
+      wx.navigateTo({
+        url: '/pages/detail/detail',
+      })
+    }
   }
 })
