04402570b1178959a6a03b2d238ade90825e5f39
lisusususuli <2363790746@qq.com>
2020-01-10 22:29:22 +0800

    购物车完成

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,256 @@
 // pages/cart/cart.js
+import touch from "../../utils/touchmoveDir.js"
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    cart: [],
+    total: {
+      price: 0,
+      count: 0,
+      isTotal: false,
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  touch: new touch(),
 
+  onLoad() { 
+    let that = this;
+    wx.getStorage({
+      key: 'cart',
+      success: function(res) {
+        let { currentCount, currentPrice } = that._payment(res.data)
+        that.setData({
+          cart: res.data,
+          total: {
+            count: currentCount,
+            price: currentPrice
+          }
+        })
+      },
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  deleteGoods(e) {
+    let currentIndex = e.currentTarget.dataset.index
+    let brandId = e.currentTarget.dataset.brand
+    let that = this
+    wx.showModal({
+      content: '确定要删除该商品吗',
+      confirmColor: '#e96560',
+      success(res) {
+        if (res.confirm) {
+          that._modifyCart(e, (ind, currentIndex, item, newList) => {
+            if (ind != currentIndex) {
+              newList.push(item)
+            }
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+  goodsAdd(e) {
+    this._modifyCart(e, (i, index, item, newList) => {
+      if (i == index) {
+        if (item.count < 50) {
+          item.count++
+        }
+      }
+      newList.push(item)
+    })
+  },
+  goodsSub(e) {
+    this._modifyCart(e, (i, index, item, newList) => {
+      if (i == index) {
+        if (item.count > 1) {
+          item.count--
+        }
+      }
+      newList.push(item)
+    })
+  },
+//增删改逻辑
+  _modifyCart(e, func) {
+    let currentIndex = e.currentTarget.dataset.index
+    let brandId = e.currentTarget.dataset.brand
+    let newStore = []
 
+    this.data.cart.forEach((store, index) => {
+      if (store.brandId == brandId) {
+        let newList = []
+        store.list.forEach((item, ind) => {
+          //传入当前遍历index，当前选中项index，当前选中项，用于赋值的新数组
+          func(ind, currentIndex, item, newList)
+        })
+        store.list = newList
+      }
+      if (store.list.length > 0) {
+        newStore.push(store)
+      }
+    })
+    let { currentCount, currentPrice } = this._payment(newStore)
+    this.setData({
+      cart: newStore,
+      total: {
+        count: currentCount,
+        price: currentPrice
+      }
+    }, () => {
+      wx.setStorage({
+        key: "cart",
+        data: newStore
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  touchstart(e) {
+    //开始触摸时 重置所有删除,让所有isTouchMove都是false，达到页面只有一个元素可以滑动删除得效果
+    let data = this.touch._touchstart(e, this.data.cart)
+    this.setData({
+      cart: data
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  touchmove(e) {
+    let data = this.touch._touchmove(e, this.data.cart)
+    this.setData({
+      cart: data
+    })
   },
+//二级全选
+  checkedAll(e){
+    let index = e.target.dataset.index
+   
+    let isChecked = !this.data.cart[index].checked
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    this.data.cart[index].checked = isChecked
+    this.data.cart[index].list.forEach((item , i )=>{
+      this.data.cart[index].list[i].checked = isChecked
+    })
+    //判断是不是全选中了，修改最底部全选
+    let isAll = this._isAll(this.data.cart)
+    //计算价格
+    let { currentCount, currentPrice } = this._payment(this.data.cart)
+    this.setData({
+      cart: this.data.cart,
+      total: {
+        isTotal: isAll,
+        count: currentCount,
+        price: currentPrice
+      }
+    },() => {
+      wx.setStorage({
+        key: "cart",
+        data: this.data.cart
+      })
+    })
   },
+  //单选
+  checkedOne(e){
+    
+    let currentIndex = e.target.dataset.index
+    let brandId = e.target.dataset.brandid
+    let checkedCount = 0
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    let cart= this.data.cart
 
+    cart.forEach((store,index)=>{
+      if (store.brandId == brandId){
+        store.list.forEach((item,i)=>{
+          if (currentIndex == i){
+            cart[index].list[i].checked = !cart[index].list[i].checked
+          }
+          if (cart[index].list[i].checked) {
+            checkedCount++
+          }
+        })
+        if (checkedCount == store.list.length){
+          cart[index].checked = true
+        } else {
+          cart[index].checked = false
+        }
+      }
+    })
+    //判断是不是全选中了，修改最底部全选
+    let isAll = this._isAll(this.data.cart)
+    //计算价格
+    let { currentCount, currentPrice } = this._payment(cart)
+    this.setData({
+      cart: cart,
+      total: {
+        isTotal: isAll,
+        count: currentCount,
+        price: currentPrice,
+        
+      }
+    }, () => {
+      wx.setStorage({
+        key: "cart",
+        data: cart
+      })
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  //底部一级全选框
+  allgoodsCheck(){
+    let allCheck = !this.data.total.isTotal
+    //全选或全反选
+    this.data.cart.forEach((store,index) => {
+      this.data.cart[index].checked = allCheck
+      store.list.forEach((item, i) => {
+        this.data.cart[index].list[i].checked = allCheck
+      })
+    })
+    //计算价格
+    let { allCount, allPrice } = this._payment(this.data.cart)
+    if (!allCheck){
+      allCount= 0,
+      allPrice =0
+    }
+    //更新，存库
+    this.setData({
+      cart:this.data.cart,
+      total:{
+        isTotal: allCheck,
+        count: allCount,
+        price: allPrice
+      }
+    },()=>{
+      wx.setStorage({
+        key: "cart",
+        data: this.data.cart
+      })
+    })
   },
+  //计算价格数量
+  _payment(cart){
+    let allCount = 0;
+    let currentCount = 0;
+    let allPrice = 0;
+    let currentPrice =0
+    cart.forEach((store) => {
+      store.list.forEach((item,i)=>{
+        allCount += item.count
+        allPrice += item.count * item.proPrice
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+        if (item.checked){
+          currentCount += item.count
+          currentPrice += item.count * item.proPrice
+        }
+      })
+    })
+    return {
+      allCount, allPrice, currentCount, currentPrice
+    }
+  },
+  //判断购物车是否全部选中，是则令底部全选为true
+  _isAll(cart){
+    let allBoolean = cart.every((store, index) => {
+      return store.list.every((item, i) => {
+        return item.checked == true
+      })
+    })
+    return allBoolean;
   }
 })
--- pages/myjx/myjx.js
+++ pages/myjx/myjx.js
@@ -1,25 +1,25 @@
 // pages/myjx/myjx.js
 Page({
-
   login(){
     console.log("login")
-    // wx.login({
-    //   success(res) {
-    //     if (res.code) {
-    //       console.log(res.code)
-    //       //发起网络请求
-    //       wx.request({
-    //         url: 'https://lisusususuli.top/login',
-    //         data: {
-    //           code: res.code
-    //         }
-    //       })
-
-    //     } else {
-    //       console.log('登录失败！' + res.errMsg)
-    //     }
-    //   }
-    // })
+    wx.login({
+      success(res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'https://lisusususuli.top/users/login',
+            data: {
+              code: res.code
+            },
+            success:(data)=>{
+              console.log(data)
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
   },
 
   toAllList(){
